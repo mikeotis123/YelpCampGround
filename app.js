d27cac11975aba430d1ce4a0fdb0c5575c33ea9b
mikeotis123 <michael.otis2006@gmail.com>
2018-01-26 11:10:24 -0600

    Added the ability to add campgrounds to the site

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
+ var campgrounds = [
+{name: 'Salmon Creek', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg23JSvRfefg17Pn5pFJ64R_7p6FlZlMu4vp9WgT_rdEwwtuMEAA'},
+{name: 'Granite hill', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAlt7fW1YA1_B462rAyHQMfxZbWjzFyGaoTNbAiUqVSSVVq2R8'},
+{name: 'Mountain of youth', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpbmkCU4Z1QkQ5oEAPTv66cQ7026lMSuy9sQ7exm3aS_Pc4QMsbA'}
+    ];
 
 app.get('/', function(req, res){
     res.render('landing');
 });
 
+
 app.get('/campgrounds', function(req, res){
-    var campgrounds = [
-{name: 'Salmon Creek', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg23JSvRfefg17Pn5pFJ64R_7p6FlZlMu4vp9WgT_rdEwwtuMEAA'},
-{name: 'Granite hill', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAlt7fW1YA1_B462rAyHQMfxZbWjzFyGaoTNbAiUqVSSVVq2R8'},
-{name: 'Mountain of youth', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpbmkCU4Z1QkQ5oEAPTv66cQ7026lMSuy9sQ7exm3aS_Pc4QMsbA'}
-    ]
     res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+
+app.post('/campgrounds', function(req, res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image}
+	campgrounds.push(newCampground);
+	res.redirect('/campgrounds');
+});
+
+
+app.get('/campgrounds/new', function(req, res){
+	res.render('new.ejs');
 });
 
 
