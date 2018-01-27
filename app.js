d1c47745a0c2c5b77d36375c7bf88749b344e260
mikeotis123 <michael.otis2006@gmail.com>
2018-01-27 15:39:19 -0600

    added an older version of bootstrap that goes along with the tutorial.  Now all the styles are showing up.  Also finished the styleing for the form to create a new campground

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set('view engine', 'ejs');
  var campgrounds = [
 {name: 'Salmon Creek', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg23JSvRfefg17Pn5pFJ64R_7p6FlZlMu4vp9WgT_rdEwwtuMEAA'},
 {name: 'Granite hill', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAlt7fW1YA1_B462rAyHQMfxZbWjzFyGaoTNbAiUqVSSVVq2R8'},
+{name: 'Mountain of youth', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpbmkCU4Z1QkQ5oEAPTv66cQ7026lMSuy9sQ7exm3aS_Pc4QMsbA'},
+{name: 'Salmon Creek', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg23JSvRfefg17Pn5pFJ64R_7p6FlZlMu4vp9WgT_rdEwwtuMEAA'},
+{name: 'Granite hill', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAlt7fW1YA1_B462rAyHQMfxZbWjzFyGaoTNbAiUqVSSVVq2R8'},
 {name: 'Mountain of youth', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpbmkCU4Z1QkQ5oEAPTv66cQ7026lMSuy9sQ7exm3aS_Pc4QMsbA'}
     ];
 
